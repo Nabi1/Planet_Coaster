ce905bdea6d3050f31de2b71144deacd53600f08
Cedric <cedric.seng@gmail.com>
2018-06-28 17:17:37 +0200

    merging

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import FiltersImg from './components/FiltersImg';
-import FiltersText from './components/FiltersText';
-import FetchData from './components/FetchData';
-import ShowResults from './components/ShowResults';
-import MapGrid from './components/MapGrid';
+// import FiltersImg from './components/FiltersImg';
+// import FiltersText from './components/FiltersText';
+// import FetchData from './components/FetchData';
+// import ShowResults from './components/ShowResults';
+// import MapGrid from './components/MapGrid';
+import AllComponents from './components/AllComponents';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FiltersText />
+      <h1>BIENVENUE A DESPAIRWORLD</h1>
+        <AllComponents/>
+        {/* <FiltersText />
         <FiltersImg />
         <ShowResults />
-        <FetchData />
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MapGrid />
+        <FetchData /> */}
+        {/* <MapGrid /> */}
       </div>
     );
   }
---
+++ front/src/components/AllComponents.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import MapGrid from './MapGrid';
+import ShowResults from './ShowResults';
+import FiltersImg from './FiltersImg';
+import FiltersText from './FiltersText';
+
+class AllComponents extends Component {
+    render() {
+        return (
+            <div>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12} md={2}>
+                            <FiltersText/>
+                            <ShowResults/>
+                            <FiltersImg/>
+                        </Grid>
+                        
+                        <Grid item xs={12} md={10}>
+                           <MapGrid/>
+                        </Grid>
+                    </Grid> 
+            </div>
+    
+        )
+    }
+}
+
+export default AllComponents;
